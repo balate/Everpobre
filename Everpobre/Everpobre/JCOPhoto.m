beee4b551b4b9c52348523f809384d54f1bb8b22
Javier Contreras <javi_ocon@yahoo.es>
2016-08-27 11:59:35 +0200

    Crear fault para aviso de warni Memory del sistema. Este se genera con subcribiendo el metodo de crear imagen con una Notificacion para liberar la imagen.

--- Everpobre/Everpobre/JCOPhoto.m
+++ Everpobre/Everpobre/JCOPhoto.m
@@ -47,7 +47,36 @@
     
     p.imageData = UIImagePNGRepresentation(image);
     
+    
+    
+    //Alta en notificacion de aviso de memoria
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    
+    [nc addObserver:p selector:@selector(notyfyThatDidReceiveMemoryWarning:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+    
     return p;
 
 }
+
+
+
+#pragma mark - Notifications
+
+//UIApplicationDidReceiveMemoryWarningNotification
+-(void)notyfyThatDidReceiveMemoryWarning:(NSNotification *) notification{
+
+    _image = nil;
+}
+
+
+
+#pragma mark - Lifecycle
+
+-(void) dealloc {
+
+    //Se da de baja de la notificacion
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self];
+}
+
 @end
