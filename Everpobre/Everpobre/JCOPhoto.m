bb3e50d09c969a0cb32ba25b7c65567a11c01205
Javier Contreras <javi_ocon@yahoo.es>
2016-08-28 20:22:54 +0200

    Simplificación de clase photo

--- Everpobre/Everpobre/JCOPhoto.m
+++ Everpobre/Everpobre/JCOPhoto.m
@@ -8,35 +8,21 @@
 
 @implementation JCOPhoto
 
-@synthesize image = _image;
 
 #pragma mark - Properties
-
 //Setter
 -(void) setImage:(UIImage *) image{
-
-    //Guardar en la variable de instancia
-    _image = image;
-
 
     //sincronizar con imageData
     self.imageData =UIImagePNGRepresentation(image);
 }
 
 //Getter
-//Se va hacer con la tecnica de carga perezosa (reduciendo el tiempo de memoria en el tiempo)
 -(UIImage *)image {
 
-    if(_image ==nil){
+    return [UIImage imageWithData:self.imageData];
     
-        _image = [UIImage imageWithData:self.imageData];
-        
-    }
-    return _image;
 }
-
-
-
 
 
 #pragma mark - class method
@@ -47,36 +33,9 @@
     
     p.imageData = UIImagePNGRepresentation(image);
     
-    
-    
-    //Alta en notificacion de aviso de memoria
-    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-    
-    [nc addObserver:p selector:@selector(notyfyThatDidReceiveMemoryWarning:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
-    
     return p;
-
 }
 
 
-
-#pragma mark - Notifications
-
-//UIApplicationDidReceiveMemoryWarningNotification
--(void)notyfyThatDidReceiveMemoryWarning:(NSNotification *) notification{
-
-    _image = nil;
-}
-
-
-
-#pragma mark - Lifecycle
-
--(void) dealloc {
-
-    //Se da de baja de la notificacion
-    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-    [nc removeObserver:self];
-}
 
 @end
