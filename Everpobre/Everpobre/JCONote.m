8e6d3b3f8b05779f1b7a0d82c6c636b9cc9af0d8
Javier Contreras <javi_ocon@yahoo.es>
2016-08-30 18:47:05 +0200

    Implementar ciclo de vida de NSManagedObject en Note

--- Everpobre/Everpobre/JCONote.m
+++ Everpobre/Everpobre/JCONote.m
@@ -4,11 +4,18 @@
 
 // Private interface goes here.
 
++(NSArray * ) observableKeyNames;
+
 @end
 
 @implementation JCONote
 
 // Custom logic goes here.
+
++(NSArray *) observableKeyNames {
+
+    return @[@"name", @"creationDate",@"notebook",@"photo"];
+}
 
 +(instancetype) noteWhitNane:(NSString *)name
                     notebook:(JCONoteBook *)notebook
@@ -22,5 +29,66 @@
     
     return note;
 }
+
+
+#pragma mark -KVO
+
+//Observamos cualquier de cuaquier propiedad de notebook
+-(void) setupKVO{
+    
+    for (NSString *key in [JCONote observableKeyNames]) {
+        
+        [self addObserver:self forKeyPath:key
+                  options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+                  context:NULL];
+    }
+    
+}
+
+//nos damos de baja del observer
+-(void)tearDownKVO{
+    
+    for (NSString *key in [JCONote observableKeyNames]){
+        
+        [self removeObserver:self
+                  forKeyPath:key];
+    }
+}
+
+//modifica fecha de modificacion si hay algun cambio
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSString *,id> *)change
+                      context:(void *)context{
+    
+    self.modificationDate = [NSDate date];
+    
+}
+
+
+#pragma mark - LifeCycle
+//ciclo de vida de NSManagedObject
+-(void) awakeFromInsert{
+    
+    [super awakeFromInsert];
+    [self setupKVO];
+    
+}
+
+//creaccion
+-(void)awakeFromFetch{
+    
+    [super awakeFromFetch];
+    [self setupKVO];
+}
+
+
+//destrucción
+-(void) willTurnIntoFault{
+    
+    [super willTurnIntoFault];
+    [self tearDownKVO];
+}
+
 
 @end
