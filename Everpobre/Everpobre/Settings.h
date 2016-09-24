665f8c814d11ba0ce3088488b40f4f97f6178578
Javier Contreras <javi_ocon@yahoo.es>
2016-09-24 11:12:23 +0200

    mogenerator-1.30.1

---
+++ Everpobre/Everpobre/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  Everpobre
+//
+//  Created by Javier Contreras on 1/9/16.
+//  Copyright © 2016 Ocon. All rights reserved.
+//
+
+#ifndef Settings_h
+#define Settings_h
+
+
+#endif /* Settings_h */
